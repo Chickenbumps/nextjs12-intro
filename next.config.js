ad6b06968aed93ccb3a21b5bf067fdaa88dd2f13
Chickenbumps <whgotjd46@gmail.com>
2023-02-24 23:21:42 +0900

    #8 Nextjs concept: redirects, rewrites with next.config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,27 @@
 /** @type {import('next').NextConfig} */
+
+const API_KEY = process.env.MOVIE_API_KEY;
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/old/:path*",
+        destination: "/new/:path*",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
