c69b5ea0040f66515d4355b94fa1c9b3bc970183
Chickenbumps <whgotjd46@gmail.com>
2023-03-02 22:54:46 +0900

    #16 Nextjs concept: getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,8 @@
-import { GetServerSideProps } from "next";
+import {
+  GetServerSideProps,
+  GetStaticProps,
+  InferGetServerSidePropsType,
+} from "next";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
@@ -6,12 +10,13 @@ interface PageProps {
   results: Object[];
 }
 
-export default function Home({ results }: PageProps) {
+export default function Home({
+  results,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const router = useRouter();
   const onClickMovie = ({ id, title }: { id: number; title: string }) => {
     router.push(`/movies/${title}/${id}`);
   };
-
   return (
     <div className="container">
       {results?.map((movie: any) => (
@@ -52,8 +57,23 @@ export default function Home({ results }: PageProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps<PageProps> = async () => {
-  const { results }: any = await (
+// page가 요청받을 때 마다 pre-rendering
+// 동적데이터 처리
+export const getServerSideProps: GetServerSideProps<PageProps> = async (
+  context
+) => {
+  const { results }: PageProps = await (
+    await fetch(`${process.env.DEV_HOST_NAME}/api/movies`)
+  ).json();
+  return {
+    props: { results },
+  };
+};
+
+// 빌드 시에 딱 한번 pre-rendering 되고 바로 static file로 빌드되어 이후 수정 불가.
+// 고정데이터 처리
+export const getStaticProps: GetStaticProps<PageProps> = async () => {
+  const { results }: PageProps = await (
     await fetch(`${process.env.DEV_HOST_NAME}/api/movies`)
   ).json();
   return {
